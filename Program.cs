737a731dca847e0f80a053a30db6edfa500b3ca1
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-06-09 13:56:49 -0500

    started getting my new C project set up.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace RPSLS_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool playAgain = true;
+            do
+            {
+                Run game = new Run();
+                game.run_game();
+                Console.WriteLine("Would you like to play again? y/n");
+                string userInput = Console.ReadLine();
+                if(userInput != "y")
+                {
+                    playAgain = false;
+                }
+            }
+            while (playAgain == true);
+        }
+    }
+}
---
+++ run.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS_C_Sharp
+{
+    class Run
+    {
+        //Member Variables
+        //public class playerOne;
+        //computer = new Computer();
+        //int round = 0;
+
+        //Constructor
+        public Run()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        //Member Methods
+        public void run_game()
+        {
+            Console.WriteLine("Game is Running!");
+        }
+
+    }
+}
